32656d8b241506f34271ea6b9f0bda9703abe492
Chase Southard <chase.southard@gmail.com>
2016-01-16 23:01:21 -0500

    working!; add icon images

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -5,3 +5,30 @@ chrome.runtime.onInstalled.addListener(details => {
 });
 
 console.log('\'Allo \'Allo! Event Page');
+
+const messages = ['Yo! Watch it on the tabs will ya?!',
+                  'Easy on the tabs, man.',
+                  'Sometimes you get the tab, sometimes the tab gets you.',
+                  'Oh so many tabs. So many.',
+                  'Stop. Stop this tabuse.',
+                  'Seriously?! Another tab?',
+                  'So this is your plan? Another tab. Great.'];
+
+function getMessage() {
+   return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function showNotification() {
+  return new Notification('Excessive Tab Reminder', {body: getMessage(), icon: 'images/noun_158509_48.png'});
+}
+
+if (window.Notification) {
+  chrome.tabs.onCreated.addListener(function() {
+    chrome.windows.getAll({populate: true}, function(allWindows) {
+      var tabCount = allWindows[0].tabs.length;
+      if (tabCount > 9) {
+        showNotification();
+      }
+    });
+  });
+}
